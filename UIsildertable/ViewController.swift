d41e21128fe68e5c5f1b45e3b58acc0494b31ae5
D7703_23 <d7703_23@D7703-23ui-iMac.local>
2018-05-16 14:19:44 +0900

    add

--- UIsildertable/ViewController.swift
+++ UIsildertable/ViewController.swift
@@ -8,18 +8,39 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    @IBOutlet weak var myslider: UISlider!
+    @IBOutlet weak var mytabelview: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        //mySlider.setValue(0.0, animated: true)
+       mytabelview.dataSource = self
+       mytabelview.delegate = self
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func sliderMoved(_ sender: Any) {
+        
+        print("slider vlaue = \(Int(myslider.value ))")
+        
+        
+        mytabelview.reloadData()
     }
-
-
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 100
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let identifier = "RE"
+        let cell = mytabelview.dequeueReusableCell(withIdentifier: identifier, for: indexPath)
+        
+        let tableValue = String(Int(myslider.value) + indexPath.row)
+        cell.textLabel?.text = tableValue
+        return  cell
+    }
+    
 }
+
 
